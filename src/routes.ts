efaf3f07a83e93f9b7fdf595a95ab1e478140c04
Zé Marcos <jose.ferreira@xpm.local>
2022-06-27 15:12:03 -0300

    First Commit

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import UserController from "./controllers/UserController";
+
+const router = express.Router();
+
+router.post("/users", UserController.create);
+router.get("/users", UserController.findAll);
+router.get("/users/:userID", UserController.findOne);
+router.put("/users/:userID", UserController.update);
+router.delete("/users/:userID", UserController.destroy);
+
+export { router };
